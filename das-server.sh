1e445d6848490afb2252f6ab99e10b501f3f1760
fangyeming <fangyemimgzz@gmail.com>
2024-09-23 20:56:43 +0800

    Refactor nodeConfig.json and das-server.sh for improved configuration management
    - Removed redundant "dangerous" section and moved it to a more appropriate location in nodeConfig.json.
    - Simplified the "private-key" field in the "parent-chain-wallet" section.
    - Adjusted the das-server.sh script to streamline the command execution and ensure proper configuration of S3 storage parameters.

--- das-server.sh
+++ das-server.sh
@@ -13,29 +13,28 @@ PARENT_CHAIN_NODE_URL=$(jq -r '.node."data-availability"."parent-chain-node-url"
 # Check if the "data-availability" key exists
 if jq -e '.node | has("data-availability")' "$NODE_CONFIG" >/dev/null; then
     # Start daserver with the new command
-    /usr/local/bin/daserver \
-        --data-availability.parent-chain-node-url "$PARENT_CHAIN_NODE_URL" \
-        --enable-rpc \
-        --rpc-addr '0.0.0.0' \
-        --enable-rest \
-        --rest-addr '0.0.0.0' \
-        --log-level 3 \
-        --data-availability.local-db-storage.enable \
-        --data-availability.local-db-storage.data-dir /home/user/das-data \
-        --data-availability.local-db-storage.discard-after-timeout=false \
-        --data-availability.key.key-dir /home/user/.arbitrum/keys \
-        --data-availability.local-cache.enable \
-        --data-availability.sequencer-inbox-address "$SEQUENCER_INBOX_ADDRESS"
-        --data-availability.s3-storage.enable \
-        --data-availability.s3-storage.access-key "<YOUR ACCESS KEY>" \
-        --data-availability.s3-storage.bucket "<YOUR BUCKET>" \
-        --data-availability.s3-storage.region "<YOUR REGION>" \
-        --data-availability.s3-storage.secret-key "<YOUR SECRET KEY>" \
-        --data-availability.s3-storage.object-prefix "<YOUR OBJECT KEY PREFIX>/" \
-        --data-availability.s3-storage.discard-after-timeout false \
-        --data-availability.local-file-storage.enable \
-        --data-availability.local-file-storage.data-dir /home/user/data/das-data
-
+/usr/local/bin/daserver \
+    --data-availability.parent-chain-node-url "$PARENT_CHAIN_NODE_URL" \
+    --enable-rpc \
+    --rpc-addr '0.0.0.0' \
+    --enable-rest \
+    --rest-addr '0.0.0.0' \
+    --log-level 3 \
+    --data-availability.local-db-storage.enable \
+    --data-availability.local-db-storage.data-dir /home/user/das-data \
+    --data-availability.local-db-storage.discard-after-timeout=false \
+    --data-availability.key.key-dir /home/user/.arbitrum/keys \
+    --data-availability.local-cache.enable \
+    --data-availability.sequencer-inbox-address "$SEQUENCER_INBOX_ADDRESS" \
+    --data-availability.s3-storage.enable \
+    --data-availability.s3-storage.access-key "" \
+    --data-availability.s3-storage.bucket "" \
+    --data-availability.s3-storage.region "" \
+    --data-availability.s3-storage.secret-key "" \
+    --data-availability.s3-storage.object-prefix "das-data/" \
+    --data-availability.s3-storage.discard-after-timeout false \
+    --data-availability.local-file-storage.enable \
+    --data-availability.local-file-storage.data-dir /home/user/data/das-data
 else
     # Log a message and exit if "data-availability" key doesn't exist
     echo "You're running in Rollup mode. No need for das-server, so exiting the container ..."
