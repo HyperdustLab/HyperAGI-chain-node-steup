b6766f316136b7e9df7fc12c54c2347006a39aa5
fangyeming <fangyemimgzz@gmail.com>
2024-09-24 12:45:54 +0800

    Refactor nodeConfig and das-server.sh for updated configurations
    - Removed sequencer and delayed-sequencer configurations from `nodeConfig.json`.
    - Updated `rest-aggregator` URL and added `feed` input URL in `nodeConfig.json`.
    - Changed `forwarding-target` and updated caching settings in `nodeConfig.json`.
    - Added key generation steps in `das-server.sh` if keys do not exist.
    - Simplified `daserver` command in `das-server.sh` by removing unused parameters.

--- das-server.sh
+++ das-server.sh
@@ -4,6 +4,11 @@
 ORBIT_CONFIG="/home/user/.arbitrum/orbitSetupScriptConfig.json"
 NODE_CONFIG="/home/user/.arbitrum/nodeConfig.json"
 
+# Create keys directory and generate keys if they don't exist
+mkdir -p /home/user/.arbitrum/keys && \
+[ ! -f /home/user/.arbitrum/keys/das_bls.pub ] && datool keygen --dir /home/user/.arbitrum/keys; \
+[ ! -f /home/user/.arbitrum/keys/ecdsa.pub ] && datool keygen --dir /home/user/.arbitrum/keys --ecdsa;
+
 # Read sequencerInbox address from the orbitSetupScriptConfig JSON file
 SEQUENCER_INBOX_ADDRESS=$(jq -r '.sequencerInbox' "$ORBIT_CONFIG")
 
@@ -13,28 +18,19 @@ PARENT_CHAIN_NODE_URL=$(jq -r '.node."data-availability"."parent-chain-node-url"
 # Check if the "data-availability" key exists
 if jq -e '.node | has("data-availability")' "$NODE_CONFIG" >/dev/null; then
     # Start daserver with the new command
-/usr/local/bin/daserver \
-    --data-availability.parent-chain-node-url "$PARENT_CHAIN_NODE_URL" \
-    --enable-rpc \
-    --rpc-addr '0.0.0.0' \
-    --enable-rest \
-    --rest-addr '0.0.0.0' \
-    --log-level 3 \
-    --data-availability.local-db-storage.enable \
-    --data-availability.local-db-storage.data-dir /home/user/das-data \
-    --data-availability.local-db-storage.discard-after-timeout=false \
-    --data-availability.key.key-dir /home/user/.arbitrum/keys \
-    --data-availability.local-cache.enable \
-    --data-availability.sequencer-inbox-address "$SEQUENCER_INBOX_ADDRESS" \
-    --data-availability.s3-storage.enable \
-    --data-availability.s3-storage.access-key "" \
-    --data-availability.s3-storage.bucket "" \
-    --data-availability.s3-storage.region "" \
-    --data-availability.s3-storage.secret-key "" \
-    --data-availability.s3-storage.object-prefix "das-data/" \
-    --data-availability.s3-storage.discard-after-timeout false \
-    --data-availability.local-file-storage.enable \
-    --data-availability.local-file-storage.data-dir /home/user/data/das-data
+    /usr/local/bin/daserver \
+        --data-availability.parent-chain-node-url "$PARENT_CHAIN_NODE_URL" \
+        --enable-rpc \
+        --rpc-addr '0.0.0.0' \
+        --enable-rest \
+        --rest-addr '0.0.0.0' \
+        --log-level 3 \
+        --data-availability.local-db-storage.enable \
+        --data-availability.local-db-storage.data-dir /home/user/das-data \
+        --data-availability.local-db-storage.discard-after-timeout=true \
+        --data-availability.key.key-dir /home/user/.arbitrum/keys \
+        --data-availability.local-cache.enable \
+        --data-availability.sequencer-inbox-address "$SEQUENCER_INBOX_ADDRESS"
 else
     # Log a message and exit if "data-availability" key doesn't exist
     echo "You're running in Rollup mode. No need for das-server, so exiting the container ..."
