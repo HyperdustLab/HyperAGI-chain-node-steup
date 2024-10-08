b40351c40142c519bf44fcb5fdc11930aeeb3647
fangyeming <fangyemimgzz@gmail.com>
2024-10-08 11:21:59 +0800

    Enhance data availability configuration and security
    - Removed hardcoded private key from `nodeConfig.json` for improved security.
    - Updated `das-server.sh` to include additional data availability configurations:
      - Enabled extra signature checking with a public key.
      - Increased log level for more detailed logging.
      - Added S3 storage configuration for data availability.
      - Enabled REST aggregator with eager sync to storage.
      - Configured REST aggregator URLs for synchronization.

--- das-server.sh
+++ das-server.sh
@@ -19,17 +19,28 @@ PARENT_CHAIN_NODE_URL=$(jq -r '.node."data-availability"."parent-chain-node-url"
 if jq -e '.node | has("data-availability")' "$NODE_CONFIG" >/dev/null; then
     # Start daserver with the new command
     /usr/local/bin/daserver \
+        --data-availability.extra-signature-checking-public-key /home/user/.arbitrum/keys/ecdsa.pub \
         --data-availability.parent-chain-node-url "$PARENT_CHAIN_NODE_URL" \
         --enable-rpc \
         --rpc-addr '0.0.0.0' \
         --enable-rest \
         --rest-addr '0.0.0.0' \
-        --log-level 3 \
+        --log-level 5 \
         --data-availability.local-db-storage.enable \
         --data-availability.local-db-storage.data-dir /home/user/das-data \
         --data-availability.local-db-storage.discard-after-timeout=true \
+        --data-availability.s3-storage.enable \
+        --data-availability.s3-storage.access-key "$S3_ACCESS_KEY" \
+        --data-availability.s3-storage.bucket "$S3_BUCKET" \
+        --data-availability.s3-storage.region "$S3_REGION" \
+        --data-availability.s3-storage.secret-key "$S3_SECRET_KEY" \
+        --data-availability.s3-storage.object-prefix "$S3_OBJECT_PREFIX" \
         --data-availability.key.key-dir /home/user/.arbitrum/keys \
         --data-availability.local-cache.enable \
+        --data-availability.rest-aggregator.enable \
+        --data-availability.rest-aggregator.sync-to-storage.eager \
+        --data-availability.rest-aggregator.sync-to-storage.state-dir /home/user/syncState \
+        --data-availability.rest-aggregator.urls "http://3.7.54.15:9877,http://3.110.25.201:9877" \
         --data-availability.sequencer-inbox-address "$SEQUENCER_INBOX_ADDRESS"
 else
     # Log a message and exit if "data-availability" key doesn't exist
